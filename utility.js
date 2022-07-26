d24e4b191d65fa5bbbf3478b5b44a0b93c3f3a92
DoubleUeeD <67217439+DoubleUeeD@users.noreply.github.com>
2022-07-27 00:28:02 +0700

    add_login

--- utility.js
+++ utility.js
@@ -2,7 +2,7 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU4ODI4ODU4LCJleHAiOjE2NTg4MzI0NTgsIm5iZiI6MTY1ODgyODg1OCwianRpIjoiajhhYXNKOEl5RnpaV0FBMCIsInN1YiI6NSwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.s_1wEICXf3zw26SChHUAeiKa35c32mwDSm27iXg8-tg"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU4ODUzMzE0LCJleHAiOjE2NTg4NTY5MTQsIm5iZiI6MTY1ODg1MzMxNCwianRpIjoiYVJFQ21IZE5pQTYxU091YiIsInN1YiI6NSwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.Z9Gi2TFVALKNsLaeS0nFMGahOCpAB3nQ3W7dZOkoCwg"; // put access token
 const student = {
   student_id: 63363096,
   name: 'Napat', // replace with your full name.
